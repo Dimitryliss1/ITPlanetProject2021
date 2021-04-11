5779cba20045ebe5eee8e70e2e2a31ec19fb13c8
Sergey <kurilov.sereja2012@yandex.ru>
2021-04-12 02:18:55 +0300

    переключение в мейне

--- app/src/main/java/com/example/it_planet/ArticleActivity.kt
+++ app/src/main/java/com/example/it_planet/ArticleActivity.kt
@@ -1,12 +1,13 @@
 package com.example.it_planet
 
-import android.app.ActionBar
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
@@ -15,30 +16,20 @@ import org.jsoup.select.Elements
 import java.io.IOException
 
 
-class ArticleActivity : AppCompatActivity() {
+class ArticleActivity : Activity() {
 
     private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_article)
 
         val link: String = getIntent().getStringExtra("link").toString()
-
         webView = findViewById(R.id.webView)
         webView.webViewClient = WebViewClient()
         webView.getSettings().setJavaScriptEnabled(true)
         parsers(link)
-        val actionbar = supportActionBar
-        actionbar!!.title = "View Article"
-        actionbar.setDisplayHomeAsUpEnabled(true)
-        actionbar.setDisplayHomeAsUpEnabled(true)
-
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return true
     }
 
     private fun parsers(url :String){
@@ -50,5 +41,10 @@ class ArticleActivity : AppCompatActivity() {
                 webView.loadData(stringBuilder.toString(), "text/html", null)
             }
         }).start()
+    }
+
+    fun clickBack(v: View){
+        val intent = Intent(this@ArticleActivity, MainActivity::class.java)
+        startActivity(intent)
     }
 }
