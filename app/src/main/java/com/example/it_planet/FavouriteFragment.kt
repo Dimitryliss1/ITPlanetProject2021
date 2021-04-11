a4098a16e9d6243f38ec388207bd51ecba1b2734
D3m02 <kuzdim97@gmail.com>
2021-04-12 02:15:12 +0300

    Fixit listview and fragment

--- app/src/main/java/com/example/it_planet/FavouriteFragment.kt
+++ app/src/main/java/com/example/it_planet/FavouriteFragment.kt
@@ -1,19 +1,21 @@
 package com.example.it_planet
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import org.jsoup.select.Elements
 import java.io.IOException
 
 
 class FavouriteFragment : Fragment() {
-
+    lateinit var listView: ListView
     private lateinit var webView: WebView
     override fun onCreateView(
         inflater: LayoutInflater,
--- app/src/main/java/com/example/it_planet/ListFragment.kt
+++ app/src/main/java/com/example/it_planet/ListFragment.kt
@@ -1,22 +1,60 @@
 package com.example.it_planet
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.fragment.app.Fragment
 
 
 class ListFragment : Fragment() {
+    lateinit var listView: ListView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.list_fragment, container, false)
+        val view: View = inflater.inflate(R.layout.list_fragment, container, false)
+        listView = view.findViewById(R.id.userlist)
+        val parserVKIT = ParserVKIT()
+        var strings: MutableList<String> = parserVKIT.getHtmlFromWeb()
+
+
+      val arrayAdapter = ArrayAdapter(inflater.context, android.R.layout.simple_list_item_1, strings)
+        listView.adapter = arrayAdapter
+
+           listView.setOnItemClickListener(AdapterView.OnItemClickListener { parent, view, position, id ->
+               val link: String = parent.getItemAtPosition(position).toString().substringAfter(' ')
+                   .substringBefore(' ')
+               listView.adapter = arrayAdapter
+               //  val intent = Intent(getActivity(), ArticleActivity::class.java)
+               //  startActivity(intent.putExtra("link", link))
+           })
+
+        return view
+     //   return inflater.inflate(R.layout.list_fragment, container, false)
     }
 
+    /* override fun onCreate(savedInstanceState: Bundle?) {
+         super.onCreate(savedInstanceState)
+     }
+     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+         super.onViewCreated(view, savedInstanceState)
+         listView = view.findViewById(R.id.userlist)
+
+     }
+     private fun listNews(){
+         Thread(Runnable {
+
+             runOnUiThread {
+                 listView.adapter = arrayAdapter
+             }
+         }).start()
+
+
+     }  */
 }
